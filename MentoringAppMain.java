f19a5d77c6b9d72bbb8dc5aae6c55583f358901f
srkurpad <srkurpad@gmail.com>
2019-04-13 15:28:24 -0700

    Added console input testing

--- MentoringAppMain.java
+++ MentoringAppMain.java
@@ -10,7 +10,7 @@ public class MentoringAppMain {
       
       while (sc.hasNextLine()) {
          String name = sc.nextLine();
-         String company = sc.nextLine();
+         String company = sc.nextLine().toLowerCase();
          Mentor mentor = new Mentor(company, name);
          if (!companyMentors.containsKey(company)) {
             companyMentors.put(company, new HashSet<Mentor>());
@@ -18,13 +18,42 @@ public class MentoringAppMain {
          companyMentors.get(company).add(mentor);
       }
       
-      /*for (String company: companyMentors.keySet()) {
+      for (String company: companyMentors.keySet()) {
          System.out.println("Company: " + company + " Mentors: " + companyMentors.get(company)); 
-      }*/
+      }
       
-      Mentee user = new Mentee("Audrey", "UW");
-      Matcher m = new Matcher(user, companyMentors);
-      m.reduceList();
+      
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Mentor or Mentee?");
+      String input = scan.nextLine();
+      
+      while (!input.equalsIgnoreCase("quit")) {
+         if (input.equalsIgnoreCase("mentor")) {
+            System.out.println("Name?");
+            String name = scan.nextLine();
+            System.out.println("Company?");
+            String company = scan.nextLine().toLowerCase();
+            Mentor mentor = new Mentor(company, name);
+            if (!companyMentors.containsKey(company)) {
+               companyMentors.put(company, new HashSet<Mentor>());
+            }
+            companyMentors.get(company).add(mentor);
+            System.out.println("company: " + company);
+            System.out.println("mentors in company: " + companyMentors.get(company));
+         } else if (input.equalsIgnoreCase("mentee")) {
+            System.out.println("Name?");
+            String name = scan.nextLine();
+            System.out.println("Company?");
+            String company = scan.nextLine();
+            Mentee user = new Mentee(name, company);
+            Matcher m = new Matcher(user, companyMentors);
+            m.reduceList();
+         } else {
+            System.out.println("invalid input");
+         }
+         System.out.println("Mentor or Mentee?");
+         input = scan.nextLine();
+      }
       
    
    }
