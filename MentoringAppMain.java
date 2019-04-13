cdd6b07be8bacfa37dce08126df837a2d31e88b7
srkurpad <srkurpad@gmail.com>
2019-04-13 14:11:52 -0700

    Added mentor info text file and testing class

--- MentoringAppMain.java
+++ MentoringAppMain.java
@@ -1,4 +1,30 @@
-public static void main(String[] args) {
+import java.util.*;
+import java.io.*;
+public class MentoringAppMain {
 
+   public static void main(String[] args) throws FileNotFoundException {
+      File file = new File("MentorData");
+      Scanner sc = new Scanner(file);
+      
+      Map<String, Set<Mentor>> companyMentors = new HashMap<String, Set<Mentor>>();
+      
+      while (sc.hasNextLine()) {
+         String name = sc.nextLine();
+         String company = sc.nextLine();
+         Mentor mentor = new Mentor(company, name);
+         if (!companyMentors.containsKey(company)) {
+            companyMentors.put(company, new HashSet<Mentor>());
+         }
+         companyMentors.get(company).add(mentor);
+      }
+      
+      for (String company: companyMentors.keySet()) {
+         System.out.println("Company: " + company + " Mentors: " + companyMentors.get(company)); 
+      }
+      
+      Mentee user = new Mentee("Audrey", "Facebook");
+      
+   
+   }
 
 }
